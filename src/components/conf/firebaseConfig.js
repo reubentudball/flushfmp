e61f1c47bf9f23162190f04d2047af1d0e716c6a
Reuben Tudball <reuben_tudball@hotmail.com>
2024-12-03 15:18:18 -0500

    security update

--- src/components/conf/firebaseConfig.js
+++ src/components/conf/firebaseConfig.js
@@ -1,19 +1,17 @@
-// This should have a secrets file for the API keys and stuff, fix later
 
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC_uGVgmUDEPg2sQI-kojNkxBL-h_6bEjY",
-    authDomain: "flush-d0dc1.firebaseapp.com",
-    databaseURL: "https://flush-d0dc1-default-rtdb.firebaseio.com",
-    projectId: "flush-d0dc1",
-    storageBucket: "flush-d0dc1.appspot.com",
-    messagingSenderId: "1071559938141",
-    appId: "1:1071559938141:web:4b2d90728116457bdebdb4",
-    measurementId: "G-1NVDEC21BK"
-  
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
